c8cf7d801c28df8376e399379d37f271d3df48dd
vlGoku <markolucic21@outlook.de>
2024-01-15 11:53:29 +0100

    pure functions examples

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 "use strict";
+//pure functions
+function pureFunctions(a, b) {
+    return a + b;
+}
+console.log(pureFunctions(1, 2));
+function greetMe(name) {
+    console.log(`Hallo ${name}`);
+}
+greetMe("Marko");
 //# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,13 @@
+//pure functions
+
+function pureFunctions(a: number, b: number) {
+  return a + b;
+}
+
+console.log(pureFunctions(1, 2));
+
+function greetMe(name: string) {
+  console.log(`Hallo ${name}`);
+}
+
+greetMe("Marko");
